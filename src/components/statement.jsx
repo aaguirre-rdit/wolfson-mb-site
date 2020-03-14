21c0da614f95461549f3aa6a0ac2e26e08a10b8a
Anne A <aaguirre.rdit@gmail.com>
2020-03-14 12:06:51 +0000

    updated covid statement

--- src/components/statement.jsx
+++ src/components/statement.jsx
@@ -9,10 +9,12 @@ const Statement = () => {
   return (
     <Modal show={show} onHide={handleClose} >
       <Modal.Body >
-        In light of the ongoing COVID-19 pandemic, the May Ball Presidents’ Committee would like to reassure students that May Balls are closely monitoring the situation and will follow the advice given by the central Government, Cambridge City Council, University of Cambridge, and College officials with regard to large social functions.
-        All May Balls will release a comprehensive refund policy within the next two weeks. All Balls will aim to refund the full ticket price as much as possible. Due to the overwhelming number of enquiries regarding refunds at this time, please refrain from emailing your ticketing officer unless urgent, whilst Ball Committees prepare to release these.
-        Please note that the recent cancellations of Robinson and Newnham May Balls are not related to the outbreak and these Balls will be releasing their own statements about guest refund rights
-      </Modal.Body>
+        34
+
+        In light of the ongoing COVID-19 pandemic, the May Ball Presidents’ Committee would like to reassure students that May Balls are closely monitoring the situation and will follow the advice given by the central Government, Cambridge City Council, University of Cambridge, and College officials with regard to large social functions. As it stands, all Committees are proceeding with preparations for their events and are proud of the programmes they have so far put together. We will however react to the situation appropriately as it unfolds.
+        All May Balls will release a comprehensive refund policy within the coming weeks. While Ball Committees prepare to release their respective refund policies, please refrain from emailing your ticketing officers unless urgent.
+        Please note that the recent cancellation of Newnham June Event is not related to the outbreak and the Event will be releasing its own statements about guest refund rights.
+         </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
